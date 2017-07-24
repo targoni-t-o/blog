52845dc1e29ba896c0055431fb60f43106400233
targoni <targoni.t.o@gmail.com>
2017-07-24 17:35:48 +0300

    Error correction

--- db/migrate/20170717232854_add_fields_to_users.rb
+++ db/migrate/20170717232854_add_fields_to_users.rb
@@ -1,4 +1,4 @@
-class AddFiedsToUsers < ActiveRecord::Migration[5.0]
+class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
